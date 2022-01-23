5770c43002f201cb541945a65f7af7f3403cae9f
Hrishabh Jain <hrishabh507@gmail.com>
2022-01-23 13:21:09 +0530

    Add Alan button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
-  return <div></div>;
+  useEffect(() => {
+    alanBtn({
+      key: alanKey,
+      onCommand: ({ command, articles }) => {
+        if (command === "newHeadlines") {
+          console.log(articles);
+        }
+      },
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Alan AI News Application</h1>
+    </div>
+  );
 };
 
 export default App;
