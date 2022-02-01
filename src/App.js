407dffb10713b5b9191b72b0887d0fdc128edaa4
Hrishabh Jain <hrishabh507@gmail.com>
2022-02-01 10:13:32 +0530

    Add footer and weather card

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import alanBtn from "@alan-ai/alan-sdk-web";
 import wordsToNumbers from "words-to-numbers";
 import NewsCards from "./Components/NewsCards/NewsCards";
 import icon from "./Assets/icon.png";
+import Footer from "./Components/Footer/Footer";
 
 const alanKey = process.env.REACT_APP_ALAN_KEY;
 
 const App = () => {
   const [newsArticles, setNewsArticles] = useState([]);
   const [activeArticle, setActiveArticle] = useState(-1);
+  const [weatherInfo, setWeatherInfo] = useState({});
 
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: ({ command, articles, number }) => {
+      onCommand: ({ command, articles, number, forecastInfo, aqi }) => {
         if (command === "newHeadlines") {
           setNewsArticles(articles);
           setActiveArticle(-1);
@@ -29,23 +31,65 @@ const App = () => {
           if (article) {
             window.open(article.url, "_blank");
           }
+        } else if (command === "weather") {
+          console.log(forecastInfo);
+          console.log(aqi);
+          setWeatherInfo({
+            place: forecastInfo.name,
+            temp: Math.round(forecastInfo.currTemp),
+            high: Math.round(forecastInfo.maxTemp),
+            low: Math.round(forecastInfo.minTemp),
+            desc: forecastInfo.condition,
+            air: aqi,
+          });
         }
       },
     });
   }, []);
 
   return (
-    <div>
-      <div className="header">
-        <img className="icon" src={icon} alt="icon" />
-        <h1 className="heading">NewsEra</h1>
-      </div>
+    <div className="app">
+      <div className="main-cont">
+        <div className="navbar">
+          <div className="header">
+            <img className="icon" src={icon} alt="icon" />
+            <h1 className="heading">NewsEra</h1>
+          </div>
+          {!weatherInfo.place ? (
+            <div className="weather-help">
+              Try saying: <i> Temperature in New York!</i>
+            </div>
+          ) : (
+            <div className="weather">
+              <div className="temperature">
+                <i className="temp-icon fa-solid fa-temperature-full"></i>
+                <div className="cont">
+                  <div className="value">{weatherInfo.temp}</div>
+                  <div className="unit">°C</div>
+                </div>
+              </div>
+              <div className="description">
+                <div className="place">{weatherInfo.place}</div>
+                <div className="condition">{weatherInfo.desc}</div>
+                <div className="high-low">
+                  <strong>↑</strong> {weatherInfo.high}° | <strong>↓</strong>{" "}
+                  {weatherInfo.low}°
+                  {!weatherInfo.air ? "" : `| AQI: ${weatherInfo.air}`}
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
 
-      <NewsCards
-        className="news-cards"
-        activeArticle={activeArticle}
-        articles={newsArticles}
-      />
+        <NewsCards
+          className="news-cards"
+          activeArticle={activeArticle}
+          articles={newsArticles}
+        />
+      </div>
+      <div className="footer">
+        <Footer />
+      </div>
     </div>
   );
 };
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="created-by">Made with ❤️ by Hrishabh Jain</div>
+      <div className="contact">
+        <a
+          rel="noopener noreferrer"
+          href="https://www.instagram.com/hrishabh.hj/"
+          target="_blank"
+        >
+          <i class="fa-brands fa-instagram"></i>
+        </a>
+        <a
+          rel="noopener noreferrer"
+          href="https://twitter.com/hrishabhcodes"
+          target="_blank"
+        >
+          <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a
+          rel="noopener noreferrer"
+          href="https://github.com/HrishabhCodes"
+          target="_blank"
+        >
+          <i class="fa-brands fa-github"></i>
+        </a>
+        <a
+          rel="noopener noreferrer"
+          href="https://www.linkedin.com/in/hrishabh-jain/"
+          target="_blank"
+        >
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
